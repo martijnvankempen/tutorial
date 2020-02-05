8e0c3a718bce5d220ba908fa286edc5b6473f9de
Martijn <martijn.vankempen@gmail.com>
2020-02-05 21:41:02 +0100

    Added Redis

--- app/tutorial-application/src/Controller/DefaultController.php
+++ app/tutorial-application/src/Controller/DefaultController.php
@@ -5,28 +5,56 @@ use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Car;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\RedisAdapter;
+use DateTime;
 
 class DefaultController extends AbstractController
 {
     public function indexAction()
     {
+        $client = new \Predis\Client([
+            'scheme' => 'tcp',
+            'host'   => 'redis',
+            'port'   => 6379,
+        ]);
+
+        $cacheEntryIdentifier = "cache-entry";
+
+        $cachedData = $client->get($cacheEntryIdentifier);
+        
+        if ($cachedData != null) {
+            $json = base64_decode($cachedData);
+            $response = new Response($json);
+            $response->headers->set('Content-Type', 'text/json');
+
+            return $response;
+        }
+
     	$cars = $this->getDoctrine()
         ->getRepository(Car::class)
         ->findAll();
 
-        $responseData = [];
-        $responseData['total'] = count($cars);
+        $now = new DateTime();
+
+        $carData = [];
+        $carData['total'] = count($cars);
+        $carData['time'] = $now->format('Y-m-d H:i:s');
 
         foreach ($cars as $car) {
         	$carResponse = [];
         	$carResponse['numberOfDoors'] = $car->getNumberOfDoors();
         	$carResponse['licensePlate'] = $car->getLicensePlate();
 
-        	$responseData['items'][] = $carResponse;
+        	$carData['items'][] = $carResponse;
         }
 
-		$response = new Response(json_encode($responseData));
+        $json = json_encode($carData);
+
+		$response = new Response($json);
 		$response->headers->set('Content-Type', 'text/json');
+
+        $client->set($cacheEntryIdentifier, base64_encode($json));
+        $client->expire($cacheEntryIdentifier, 10);
 
         return $response;
     }
